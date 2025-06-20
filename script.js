60af2326de029157793581f0dc0621569c61ad1a
Jose Ferreira <jmiguelf2000@gmail.com>
2025-06-20 14:56:44 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    mobileMenuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+});
