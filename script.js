eb81b3da49065848547a2701a869e90b0ce63155
Jose Ferreira <jmiguelf2000@gmail.com>
2025-06-24 18:16:18 +0100

    Removing old unused files

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const mobileMenuButton = document.getElementById('mobile-menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
-
-    mobileMenuButton.addEventListener('click', () => {
-        mobileMenu.classList.toggle('hidden');
-    });
-});
